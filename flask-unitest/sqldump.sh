86d78ca104ac966d52b96dcc9e110342f4cec854
shubham kumar Singh <annu.shubham@gmail.com>
2020-08-16 22:57:26 +0530

    Create sqldump.sh
    test

---
+++ flask-unitest/sqldump.sh
@@ -0,0 +1,14 @@
+# mysqldump -u<user> -p<password> -h <host> db > testdump.sql
+# cat  testdump.sql| sqlite3 mysqlite3.db                                                                                                     ✔ 1|0 ⎈ goldendev-eks-api-1-clientapi-z1/dnsmapper 22:52:45
+Error: near line 25: near "AUTO_INCREMENT": syntax error
+Error: near line 38: near "LOCK": syntax error
+Error: near line 41: near "UNLOCK": syntax error
+Error: near line 50: near "ENGINE": syntax error
+Error: near line 60: near "LOCK": syntax error
+Error: near line 62: no such table: alembic_version
+Error: near line 64: near "UNLOCK": syntax error
+Error: near line 73: near "AUTO_INCREMENT": syntax error
+Error: near line 92: near "LOCK": syntax error
+Error: near line 94: no such table: apps
+Error: near line 96: near "UNLOCK": syntax error
+Error: near line 105: near "AUTO_INCREMENT": syntax error
